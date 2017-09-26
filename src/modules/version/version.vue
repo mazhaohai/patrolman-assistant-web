9374091f0e20366f7d0ffde7bd80f5d6a080e129
lemon <916035003@qq.com>
2017-09-26 14:53:58 +0800

    version编写

--- src/modules/version/version.vue
+++ src/modules/version/version.vue
@@ -8,8 +8,83 @@
 				</li>
 			</ul>
 		</div>
-		<h1>Version</h1>
-		<el-button @click="visible = true">按钮</el-button>
+
+    <el-row :gutter="20">
+      <el-col :span="5"><div ><label class="input-label">版本号:</label><el-input ></el-input></div></el-col>
+      <el-col :span="5"><div ><label class="input-label">发布人:</label><el-input ></el-input></div></el-col>
+      <el-col :span="5"><div ><label class="input-label">开始时间:</label><el-date-picker
+        type="date"
+        placeholder="选择日期"
+        >
+      </el-date-picker></div></el-col>
+      <el-col :span="5"><div ><label class="input-label">结束时间:</label><el-date-picker
+        type="date"
+        placeholder="选择日期"
+        >
+      </el-date-picker></div></el-col>
+
+      <el-col :span="4" class="form-button-search">
+        <el-button type="primary" >查询</el-button>
+        <el-button type="primary" v-on:click="createVersion()" >新建</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+
+    </el-row>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+      prop="date"
+      label="序号"
+      >
+    </el-table-column>
+      <el-table-column
+        prop="name"
+        label="比较版本号"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="版本号">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="发布时间"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="发布人"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="二维码">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="安装包下载地址"
+        >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="APP访问地址"
+        >
+      </el-table-column>
+
+      <el-table-column
+        inline-template
+        prop="address"
+        width="200"
+        label="操作">
+        <template>
+          <el-button type="primary" @click="detailVersion(row)">修改</el-button>
+          <el-button type="danger" @click="deleteVersion(row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+		<!--<el-button @click="visible = true">按钮</el-button>-->
 		<el-dialog v-model="visible" title="Hello world">
 			<p>欢迎进入 version</p>
 		</el-dialog>
@@ -19,6 +94,8 @@
 
 <script>
 	import { VersionSrv } from './version.service';
+  import ElRow from "element-ui/packages/row/src/row";
+  import ElCol from "element-ui/packages/col/src/col";
 	//用户信息
 	let _userinfo = {
 		name: '',
@@ -26,11 +103,31 @@
 	};
 
 	export default {
-		data() {
+    components: {
+      ElCol,
+      ElRow},
+    data() {
 			return {
 				userinfo: _userinfo,
 				isopen: true,
-				visible: false
+				visible: false,
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
 			}
 		},
 		created() {
@@ -40,6 +137,15 @@
 			goPage(path) {
 				this.$router.push(path);
 			},
+      createVersion(row){
+        this.$router.push({ name: 'versiondetail', params: {id:row.name}})
+      },
+      detailVersion(row){
+			    console.log(row.address);
+      },
+      deleteVersion(row){
+          console.log(row)
+      },
 			getList() {
 				let params = {
 					id: 1
@@ -65,4 +171,26 @@
 
 <style>
 
+  .input-label{
+    float: left;
+    width: 20%;
+    text-align: center;
+    height: 36px;
+    line-height: 36px;
+    margin-bottom: 10px;
+
+  }
+  .el-row{
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .el-input{
+    float: right;
+    width: 80%!important;
+  }
+  .form-button-search .el-button{
+
+    float: right;
+    margin-left: 10px;
+  }
 </style>
